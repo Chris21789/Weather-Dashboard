b09bef057339624203592d5bef0abc07d6bede68
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-12 01:03:20 -0400

    made minor id changes to the html. added css for the weather symbols. made code dry with the creation of geo info class. moved api calls to 1 function. added code to add the text content from the api retrieval to the page for current day along with the use of dayjs. added the code to insert the api symbol. working on forecast.

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -3,51 +3,92 @@ const cityNameEl = document.getElementById("cityName");
 const locationBtns = document.getElementById("locationBtns");
 const fetchButton = document.getElementById("fetch-button");
 
+class Geolocation {
+  constructor(cityName, lon, lat) {
+    (this.cityName = cityName), (this.lon = lon), (this.lat = lat);
+  }
+}
+
+var now = dayjs();
+console.log(now.format('MM/DD/YYYY'));
+
+
+
+//pulls weather info using geo info
 function getWeather() {
   const geolocation = JSON.parse(localStorage.getItem("geolocation"));
   console.log(geolocation);
   console.log(geolocation.lon);
   console.log(geolocation.lat);
-  const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${geolocation.lat}&lon=${geolocation.lon}&appid=5790fe317b106c7e96164e5dc26f6fd3`;
 
-  fetch(requestUrl)
+  const currentDay = `https://api.openweathermap.org/data/2.5/weather?lat=${geolocation.lat}&lon=${geolocation.lon}&appid=5790fe317b106c7e96164e5dc26f6fd3&units=imperial`;
+
+  fetch(currentDay)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-      for (let i = 0; i < data.list[i]; i++) {
-        const name = data.list[i].wind.speed;
-        console.log(name);
-      }
+      console.log(data.weather[0].icon);
+      console.log(data.main.temp);
+      console.log(data.wind.speed);
+      console.log(data.main.humidity);
+
+      document.getElementById(
+        "cardCityName"
+      ).innerHTML = `${geolocation.cityName} ${now.format('MM/DD/YYYY')} <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
+      document.getElementById(
+        "currentTemp"
+      ).textContent = `Temp: ${data.main.temp}\u00B0F`;
+      document.getElementById(
+        "currentWind"
+      ).textContent = `Wind: ${data.wind.speed} MPH`;
+      document.getElementById(
+        "currentHum"
+      ).textContent = `Humidity: ${data.main.humidity}%`;
+    });
+
+  const forecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${geolocation.lat}&lon=${geolocation.lon}&appid=5790fe317b106c7e96164e5dc26f6fd3&units=imperial`;
+
+  fetch(forecast)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      
+      console.log(data.list);
+
+      const fiveDay = [];
+      console.log(fiveDay);
+      
+      // for (let i = 0; i <= data.list.length; i++) {
+      //   console.log(data.list);
+      //   fiveDay.push(data.list[i]);
+      //   console.log(fiveDay);
+      // }
     });
 }
 
+//on button click pulls button geo info and runs getweather
 locationBtns.addEventListener("click", function (event) {
   event.preventDefault();
 
   const element = event.target;
   console.log(element);
-  const elementLon = element.getAttribute('lon');
-  console.log(elementLon);
-  const elementLat = element.getAttribute('lat');
-  console.log(elementLat);
 
-  const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${elementLat}&lon=${elementLon}&appid=5790fe317b106c7e96164e5dc26f6fd3`;
+  const geolocation = new Geolocation(
+    element.getAttribute("cityName"),
+    element.getAttribute("lon"),
+    element.getAttribute("lat")
+  );
+  console.log(geolocation);
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      for (let i = 0; i < data.list[i]; i++) {
-        const name = data.list[i].wind.speed;
-        console.log(name);
-      }
-    });
+  localStorage.setItem("geolocation", JSON.stringify(geolocation));
+
+  getWeather();
 });
 
+// Form submission gets geo info from api, creates button and saves geo info to button and runs getweather
 locationForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -62,20 +103,23 @@ locationForm.addEventListener("submit", (event) => {
       for (let i = 0; i < data.length; i++) {
         const lon = data[i].lon;
         const lat = data[i].lat;
+        const cityName = cityNameEl.value;
         console.log(`${data[i].lat} ${data[i].lon}`);
-        const geolocation = {
-          lon: lon,
-          lat: lat,
-        };
+
+        const geolocation = new Geolocation(cityName, lon, lat);
         console.log(geolocation);
+
         localStorage.setItem("geolocation", JSON.stringify(geolocation));
 
         const btn = document.createElement("button");
         btn.textContent = `${cityNameEl.value}`;
-        btn.setAttribute("lon", lon);        
+        btn.setAttribute("cityName", cityName);
+        btn.setAttribute("lon", lon);
         btn.setAttribute("lat", lat);
 
         locationBtns.appendChild(btn);
+
+        cityNameEl.value = "";
 
         getWeather();
       }
